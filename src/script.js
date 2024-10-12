b497c1811ed65b956b7d5eeab83b1e86ada40533
Abhilash Patil <patilabhilash1046@gmail.com>
2024-10-12 18:21:57 +0530

    First Commit

---
+++ src/script.js
@@ -0,0 +1,6 @@
+
+// Minimal JavaScript to ensure fast loading
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Website fully loaded and parsed');
+    // Add any additional JavaScript functionality here
+});
